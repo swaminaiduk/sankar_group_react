af96ae6f2b9ff6fbb1eb676c5f3e50f085e8a89b
swaminaiduk <79825102+swaminaiduk@users.noreply.github.com>
2022-01-26 17:39:54 +0530

    Updates

--- static/js/6.76dbe5a8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[6],{844:function(a,s,d){}}]);
-//# sourceMappingURL=6.76dbe5a8.chunk.js.map
