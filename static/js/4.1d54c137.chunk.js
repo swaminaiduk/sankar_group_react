643f42e966d9880a5a28d966c2c3198240c6cd93
swaminaiduk <79825102+swaminaiduk@users.noreply.github.com>
2022-01-28 18:45:32 +0530

    Updates

--- static/js/4.1d54c137.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[4],{833:function(a,s,d){}}]);
-//# sourceMappingURL=4.1d54c137.chunk.js.map
---
+++ static/js/5.e4d01ad6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpSankarGroups=this.webpackJsonpSankarGroups||[]).push([[5],{592:function(p,s,a){}}]);
+//# sourceMappingURL=5.e4d01ad6.chunk.js.map
